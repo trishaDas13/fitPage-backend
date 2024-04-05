65b154a8ac9fe3a4197cc49f6ac70ab00e4ebe25
trishaDas13 <trisha.das1308@gmail.com>
2024-04-05 19:11:51 +0530

    mongoose url change

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 //todo: connect to the database
-mongoose.connect(`mongodb://127.0.0.1:27017/stock`)
+mongoose.connect(`${process.env.DATABASE_URL}`)
 .then(() => console.log("database connected"))
 .catch(() => console.log("failed to connect with database"));
 
